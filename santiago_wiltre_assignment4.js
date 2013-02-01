177f6fc2ab865a90e8fde62b2cfbaebeb237e4f4
Wiltre Santiago <wiltre@fullsail.edu>
2013-01-31 20:13:57 -0500

    Rearanging the library on the proper way as
    Ilustrated in class

--- santiago_wiltre_assignment4.js
+++ santiago_wiltre_assignment4.js
@@ -6,108 +6,109 @@
 
 // first function is. does a string follow a 123-456-7890.
 
-function verifyPhoneNumber(phoneNumber){
+var myLib = function(){ 
+
+	var verifyPhoneNumber = function (phoneNumber){
+		if (phoneNumber.length  != 12){
+			return false;
+		} else {
+			for (var i=0; i<=2; i++){
+				var validateChar=phoneNumber.charAt(i);
+				if(isNaN(validateChar) == true){
+					return false;
+				};
+			};
+		
+			if (phoneNumber.charAt(3) != "-"){
+				return false;
+			};
+		
+			for (var i=4; i<=6; i++){
+				var validateChar=phoneNumber.charAt(i);
+				if(isNaN(validateChar) == true){
+					return false;
+				};
+			};
+		
+			if (phoneNumber.charAt(7) != "-"){
+				return false;
+			};
+		
+			for (var i=8; i<=11; i++){
+				var validateChar=phoneNumber.charAt(i);
+				if(isNaN(validateChar) == true){
+					return false;
+				};
+			};			
+		};
+		return true;
 	
-	if (phoneNumber.length  != 12){
-		return false;
-	} else {
-		for (var i=0; i<=2; i++){
-			var validateChar=phoneNumber.charAt(i);
-			if(isNaN(validateChar) == true){
-				return false;
-			};
-		};
-		
-		if (phoneNumber.charAt(3) != "-"){
-			return false;
-		};
-		
-		for (var i=4; i<=6; i++){
-			var validateChar=phoneNumber.charAt(i);
-			if(isNaN(validateChar) == true){
-				return false;
-			};
-		};
-		
-		if (phoneNumber.charAt(7) != "-"){
-			return false;
-		};
-		
-		for (var i=8; i<=11; i++){
-			var validateChar=phoneNumber.charAt(i);
-			if(isNaN(validateChar) == true){
-				return false;
-			};
-		};			
 	};
-	alert(phoneNumber + "\nIs a valid phone number")
-	return true;
-	
-};
 
 // Function Email validation 
 
-function emailValidation (emailInput){
-	var dot=".";
-	var at="@";
-	var dotPosition = emailInput.indexOf(dot);
-	var lastDotPosition = emailInput.lastIndexOf(dot);
-	var atPosition = emailInput.indexOf(at);
-	var dotSomething = emailInput.lastIndexOf(dot)+2;
+	var emailValidation = function (emailInput){
+		var dot=".";
+		var at="@";
+		var dotPosition = emailInput.indexOf(dot);
+		var lastDotPosition = emailInput.lastIndexOf(dot);
+		var atPosition = emailInput.indexOf(at);
+		var dotSomething = emailInput.lastIndexOf(dot)+2;
 		
-	if (atPosition == 0 || atPosition ==-1 || lastDotPosition == -1 || (atPosition +1) >= lastDotPosition || dotSomething > (emailInput.length -1) || dotPosition == (atPosition - 1)){
-	    	return false;
+		if (atPosition == 0 || atPosition ==-1 || lastDotPosition == -1 || (atPosition +1) >= lastDotPosition || dotSomething > (emailInput.length -1) || dotPosition == (atPosition - 1)){
+	    		return false;
+		};
+		return true;
 	};
-	return true;
-};
 
 
 
 //String URL verify since the homework ask for validate if the string start with http:// or https:// I use that as a constant for the verification. 
-function urlValidation (enterString){
-	var http = enterString.search("http://");
-	var https= enterString.search("https://");
-	if (http == -1){
-		if (https == -1){
-			return false;
+	var urlValidation = function (enterString){
+		var http = enterString.search("http://");
+		var https= enterString.search("https://");
+		var blank= enterString.indexOf(" "); 
+		if (http == -1){
+			if (https == -1){
+				return false;
+			} else {
+				if (https == 0){
+					return true + " https://";
+				};
+			};		
 		} else {
-			if (https == 0){
-				return true;
+			if (http == 0 && http > bank){
+				return true + " http://";
 			};
-		
-		};		
-	} else {
-		if (http == 0){
-			return true;
 		};
 	};
-};
 
 
 //String Capitalization. Given the string the function will capitalize the first letter of each word. 
 
-function (string);
-var space = string.split(" ");
-for (i=0, k=space.length; i,j; i++)
-
-
-
-
-
-//Main Code testing the functions. 
-// Phone Number testing 
-var phoneNumber = "555-777-8888";	
-var phoneTest = verifyPhoneNumber(phoneNumber);
-console.log(phoneTest)
-
-// Email testing
-var email = "aaa@bbb.ccc";
-var emailTest= emailValidation(email);
-console.log (emailTest);
-
-// String URL testing 
-var stringTest = "https://www.hotmail.com";
-var stringTest = urlValidation(stringTest);
-console.log(stringTest);
+	//var stringUp = function (string);
+	//var space = string.split(" ");
+	//for (i=0, k=space.length; i,j; i++)
 
 	
+	//Return object below
+	return{
+		"verifyPhoneNumber" : verifyPhoneNumber,
+		"emailValidation"   : emailValidation,
+		"urlValidation"     : urlValidation,
+		//"stringUp"          : stringUp
+		
+		
+	};
+
+};
+var phoneNumber= "555-666-7777";
+var email = "hi coma@li.com"
+var url = "https://www.1800-support.com"
+
+
+var myNewLib = new myLib();
+console.log ("Is this a phone number? " + myNewLib.verifyPhoneNumber(phoneNumber));
+console.log ("Is this a valid email? " + myNewLib.emailValidation(email));
+console.log ("Is this a url and what protocol use? " + myNewLib.urlValidation(url));
+
